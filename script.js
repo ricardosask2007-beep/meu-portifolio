e2023088e863f08742896f5018196ed36427df1c
Ricardo Carvalho <rctech.devs@gmail.com>
2026-06-28 11:40:01 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const menuToggle = document.getElementById('menuToggle')
+const menuUl = document.querySelector('#menu ul')
+
+menuToggle.addEventListener('click', () => {
+    menuUl.classList.toggle('aberto')
+})
+
+document.querySelectorAll('#menu a').forEach(link => {
+    link.addEventListener('click', () => menuUl.classList.remove('aberto'))
+})
+
 const cards = document.querySelectorAll('.card')
 
 cards.forEach(card => {
